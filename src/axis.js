e18818e82e1479ba777bacac3e4af4097ebafe51
Diego Muracciole <diegomuracciole@gmail.com>
2017-12-07 02:04:11 -0300

    Rewrite methods

--- src/axis.js
+++ src/axis.js
@@ -4,48 +4,36 @@ const { flexDirectionIsColumn, flexDirectionIsRow } = require('./flex');
 const { paddingAndBorderForAxis } = require('./paddings');
 const { floatIsUndefined } = require('./utils');
 
+// ✅
 const boundAxisWithinMinAndMax = (node, axis, value, axisSize) => {
-  let min;
-  let max;
+  let min = undefined;
+  let max = undefined;
 
   if (flexDirectionIsColumn(axis)) {
-    min = Value.resolve(
-      node.style.minDimensions[Enums.DIMENSION_HEIGHT],
-      axisSize,
-    );
-    max = Value.resolve(
-      node.style.maxDimensions[Enums.DIMENSION_HEIGHT],
-      axisSize,
-    );
+    min = Value.resolve(node.style.minDimensions[Enums.DIMENSION_HEIGHT], axisSize);
+    max = Value.resolve(node.style.maxDimensions[Enums.DIMENSION_HEIGHT], axisSize);
   } else if (flexDirectionIsRow(axis)) {
-    min = Value.resolve(
-      node.style.minDimensions[Enums.DIMENSION_WIDTH],
-      axisSize,
-    );
-    max = Value.resolve(
-      node.style.maxDimensions[Enums.DIMENSION_WIDTH],
-      axisSize,
-    );
+    min = Value.resolve(node.style.minDimensions[Enums.DIMENSION_WIDTH], axisSize);
+    max = Value.resolve(node.style.maxDimensions[Enums.DIMENSION_WIDTH], axisSize);
   }
 
   let boundValue = value;
 
-  if (!floatIsUndefined(max) && max >= 0 && boundValue > max) {
+  if (!floatIsUndefined(max) && max >= 0.0 && boundValue > max) {
     boundValue = max;
   }
 
-  if (!floatIsUndefined(min) && min >= 0 && boundValue < min) {
+  if (!floatIsUndefined(min) && min >= 0.0 && boundValue < min) {
     boundValue = min;
   }
 
   return boundValue;
-};
+}
 
-const boundAxis = (node, axis, value, axisSize, widthSize) =>
-  Math.max(
-    boundAxisWithinMinAndMax(node, axis, value, axisSize),
-    paddingAndBorderForAxis(node, axis, widthSize),
-  );
+// ✅
+const boundAxis = (node, axis, value, axisSize, widthSize) => {
+  return Math.max(boundAxisWithinMinAndMax(node, axis, value, axisSize), paddingAndBorderForAxis(node, axis, widthSize));
+}
 
 module.exports = {
   boundAxisWithinMinAndMax,
