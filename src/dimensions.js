332c91b71463115d7c6292d3aad735e59187cd3b
Diego Muracciole <diegomuracciole@gmail.com>
2017-12-07 13:09:14 -0300

    Rewrite dimensions and edges

--- src/dimensions.js
+++ src/dimensions.js
@@ -5,45 +5,29 @@ const { marginForAxis } = require('./margins');
 const { paddingAndBorderForAxis } = require('./paddings');
 const { dim } = require('./constants');
 
-const resolveDimensions = node => {
-  const { dimensions, maxDimensions, minDimensions } = node.style;
-
-  // Resolve width
-  if (
-    maxDimensions[Enums.DIMENSION_WIDTH].unit !== Enums.UNIT_UNDEFINED &&
-    Value.equal(maxDimensions[Enums.DIMENSION_WIDTH], minDimensions[Enums.DIMENSION_WIDTH])
-  ) {
-    node.resolvedDimensions[Enums.DIMENSION_WIDTH] =
-      maxDimensions[Enums.DIMENSION_WIDTH];
-  } else {
-    node.resolvedDimensions[Enums.DIMENSION_WIDTH] =
-      dimensions[Enums.DIMENSION_WIDTH];
+// ✅
+const resolveDimensions = (node) => {
+  for (let dim = Enums.DIMENSION_WIDTH; dim < Enums.DIMENSION_COUNT; dim++) {
+    if (node.style.maxDimensions[dim].unit !== Enums.UNIT_UNDEFINED &&
+        Value.equals(node.style.maxDimensions[dim], node.style.minDimensions[dim])) {
+      node.resolvedDimensions[dim] = node.style.maxDimensions[dim];
+    } else {
+      node.resolvedDimensions[dim] = node.style.dimensions[dim];
+    }
   }
+}
 
-  // Resolve height
-  if (
-    maxDimensions[Enums.DIMENSION_HEIGHT].unit !== Enums.UNIT_UNDEFINED &&
-    Value.equal(maxDimensions[Enums.DIMENSION_HEIGHT],  minDimensions[Enums.DIMENSION_HEIGHT])
-  ) {
-    node.resolvedDimensions[Enums.DIMENSION_HEIGHT] =
-      maxDimensions[Enums.DIMENSION_HEIGHT];
-  } else {
-    node.resolvedDimensions[Enums.DIMENSION_HEIGHT] =
-      dimensions[Enums.DIMENSION_HEIGHT];
-  }
-};
+// ✅
+const isStyleDimDefined = (node, axis, parentSize) => {
+  return !(node.resolvedDimensions[dim[axis]].unit === Enums.UNIT_AUTO ||
+           node.resolvedDimensions[dim[axis]].unit === Enums.UNIT_UNDEFINED ||
+           (node.resolvedDimensions[dim[axis]].unit === Enums.UNIT_POINT &&
+            node.resolvedDimensions[dim[axis]].value < 0.0) ||
+           (node.resolvedDimensions[dim[axis]].unit === Enums.UNIT_PERCENT &&
+            (node.resolvedDimensions[dim[axis]].value < 0.0 || floatIsUndefined(parentSize))));
+}
 
-const isStyleDimDefined = (node, axis, parentSize) =>
-  !(
-    node.resolvedDimensions[dim[axis]].unit == Enums.UNIT_AUTO ||
-    node.resolvedDimensions[dim[axis]].unit == Enums.UNIT_UNDEFINED ||
-    (node.resolvedDimensions[dim[axis]].unit == Enums.UNIT_POINT &&
-      node.resolvedDimensions[dim[axis]].value < 0.0) ||
-    (node.resolvedDimensions[dim[axis]].unit == Enums.UNIT_PERCENT &&
-      (node.resolvedDimensions[dim[axis]].value < 0.0 ||
-        floatIsUndefined(parentSize)))
-  );
-
+// ✅
 const emptyContainerSetMeasuredDimensions = (
   node,
   availableWidth,
@@ -53,48 +37,33 @@ const emptyContainerSetMeasuredDimensions = (
   parentWidth,
   parentHeight,
 ) => {
-  const paddingAndBorderAxisRow = paddingAndBorderForAxis(
-    node,
-    Enums.FLEX_DIRECTION_ROW,
-    parentWidth,
-  );
-  const paddingAndBorderAxisColumn = paddingAndBorderForAxis(
-    node,
-    Enums.FLEX_DIRECTION_COLUMN,
-    parentWidth,
-  );
-  const marginAxisRow = marginForAxis(
-    node,
-    Enums.FLEX_DIRECTION_ROW,
-    parentWidth,
-  );
-  const marginAxisColumn = marginForAxis(
-    node,
-    Enums.FLEX_DIRECTION_COLUMN,
-    parentWidth,
-  );
+  const paddingAndBorderAxisRow =
+      paddingAndBorderForAxis(node, Enums.FLEX_DIRECTION_ROW, parentWidth);
+  const paddingAndBorderAxisColumn =
+      paddingAndBorderForAxis(node, Enums.FLEX_DIRECTION_COLUMN, parentWidth);
+  const marginAxisRow = marginForAxis(node, Enums.FLEX_DIRECTION_ROW, parentWidth);
+  const marginAxisColumn = marginForAxis(node, Enums.FLEX_DIRECTION_COLUMN, parentWidth);
 
-  node.layout.measuredDimensions[Enums.DIMENSION_WIDTH] = boundAxis(
-    node,
-    Enums.FLEX_DIRECTION_ROW,
-    widthMeasureMode == Enums.MEASURE_MODE_UNDEFINED ||
-    widthMeasureMode == Enums.MEASURE_MODE_AT_MOST
-      ? paddingAndBorderAxisRow
-      : availableWidth - marginAxisRow,
-    parentWidth,
-    parentWidth,
-  );
-  node.layout.measuredDimensions[Enums.DIMENSION_HEIGHT] = boundAxis(
-    node,
-    Enums.FLEX_DIRECTION_COLUMN,
-    heightMeasureMode == Enums.MEASURE_MODE_UNDEFINED ||
-    heightMeasureMode == Enums.MEASURE_MODE_AT_MOST
-      ? paddingAndBorderAxisColumn
-      : availableHeight - marginAxisColumn,
-    parentHeight,
-    parentWidth,
-  );
-};
+  node.layout.measuredDimensions[Enums.DIMENSION_WIDTH] =
+      boundAxis(node,
+                      Enums.FLEX_DIRECTION_ROW,
+                      (widthMeasureMode === Enums.MEASURE_MODE_UNDEFINED ||
+                       widthMeasureMode === Enums.MEASURE_MODE_AT_MOST)
+                          ? paddingAndBorderAxisRow
+                          : availableWidth - marginAxisRow,
+                      parentWidth,
+                      parentWidth);
+  node.layout.measuredDimensions[Enums.DIMENSION_HEIGHT] =
+      boundAxis(node,
+                      Enums.FLEX_DIRECTION_COLUMN,
+                      (heightMeasureMode == Enums.MEASURE_MODE_UNDEFINED ||
+                       heightMeasureMode == Enums.MEASURE_MODE_AT_MOST)
+                          ? paddingAndBorderAxisColumn
+                          : availableHeight - marginAxisColumn,
+                      parentHeight,
+                      parentWidth);
+}
+
 
 module.exports = {
   resolveDimensions,
--- src/edges.js
+++ src/edges.js
@@ -1,25 +1,19 @@
 const Enums = require('./enums');
 const Value = require('./value');
 
+// ✅
 const computedEdgeValue = (edges, edge, defaultValue) => {
   if (edges[edge].unit !== Enums.UNIT_UNDEFINED) {
     return edges[edge];
   }
 
-  if (
-    (edge === Enums.EDGE_TOP || edge === Enums.EDGE_BOTTOM) &&
-    edges[Enums.EDGE_VERTICAL].unit !== Enums.UNIT_UNDEFINED
-  ) {
+  if ((edge === Enums.EDGE_TOP || edge === Enums.EDGE_BOTTOM) &&
+      edges[Enums.EDGE_VERTICAL].unit !== Enums.UNIT_UNDEFINED) {
     return edges[Enums.EDGE_VERTICAL];
   }
 
-  if (
-    (edge === Enums.EDGE_LEFT ||
-      edge === Enums.EDGE_RIGHT ||
-      edge === Enums.EDGE_START ||
-      edge === Enums.EDGE_END) &&
-    edges[Enums.EDGE_HORIZONTAL].unit !== Enums.UNIT_UNDEFINED
-  ) {
+  if ((edge === Enums.EDGE_LEFT || edge === Enums.EDGE_RIGHT || edge === Enums.EDGE_START || edge === Enums.EDGE_END) &&
+      edges[Enums.EDGE_HORIZONTAL].unit !== Enums.UNIT_UNDEFINED) {
     return edges[Enums.EDGE_HORIZONTAL];
   }
 
@@ -28,11 +22,11 @@ const computedEdgeValue = (edges, edge, defaultValue) => {
   }
 
   if (edge === Enums.EDGE_START || edge === Enums.EDGE_END) {
-    return new Value();
+    return Value.undefined();
   }
 
   return defaultValue;
-};
+}
 
 module.exports = {
   computedEdgeValue,
