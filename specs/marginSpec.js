487787de0509dd63c715bb7e0cf3ddef27a93041
Diego Muracciole <diegomuracciole@gmail.com>
2017-12-16 15:27:50 -0300

    Add more margin spec

--- specs/marginSpec.js
+++ specs/marginSpec.js
@@ -163,6 +163,14 @@ describe('Margins', () => {
     flexboxNode.setMarginPercent(yoga.EDGE_START, 5);
 
     expect(yogaNode.getMargin(yoga.EDGE_START)).toEqual(flexboxNode.getMargin(flexbox.EDGE_START));
+
+    const root = yoga.Node.create();
+    root.setWidth(100);
+    root.setHeight(100);
+    root.setMarginPercent(yoga.EDGE_START, 10);
+    root.calculateLayout(100, 100, yoga.DIRECTION_LTR);
+
+    console.log(root.getComputedMargin(yoga.EDGE_RIGHT));
   });
 
   test('should set end percent margin', () => {
@@ -170,5 +178,22 @@ describe('Margins', () => {
     flexboxNode.setMarginPercent(yoga.EDGE_END, 5);
 
     expect(yogaNode.getMargin(yoga.EDGE_END)).toEqual(flexboxNode.getMargin(flexbox.EDGE_END));
+  });
+
+  test.skip('computed layout margin', () => {
+    const root = flexbox.Node.create();
+    root.setWidth(100);
+    root.setHeight(100);
+    root.setMarginPercent(flexbox.EDGE_START, 10);
+
+    root.calculateLayout(100, 100, flexbox.DIRECTION_LTR);
+
+    expect(root.getComputedMargin(flexbox.EDGE_LEFT)).toBe(10);
+    expect(root.getComputedMargin(flexbox.EDGE_RIGHT)).toBe(0);
+
+    root.calculateLayout(100, 100, flexbox.DIRECTION_RTL);
+
+    expect(root.YGNodeLayoutGetMargin(flexbox.EDGE_LEFT)).toBe(0);
+    expect(root.YGNodeLayoutGetMargin(flexbox.EDGE_RIGHT)).toBe(10);
   });
 });
